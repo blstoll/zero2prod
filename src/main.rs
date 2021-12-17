d97568644e14bba71da2f023a953928621248bc1
Levi Stoll <effnish@crispin.byref.dev>
2021-12-16 22:07:13 -0700

    Updated integration test to use random port

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
+use std::net::TcpListener;
+
 use lib::run;
 
 mod lib;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    run()?.await
+    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind random port");
+    run(listener)?.await
 }
